77c5c9bef2fea8e507051d0d9805aa69df078559
iamhanantariq <110006315+iamhanantariq@users.noreply.github.com>
2023-01-14 23:45:45 +0000

    search

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -12,10 +12,10 @@ def index(request):
     })
 
 def entry(request, title):
-    all = util.list_entries()
-    if title.upper() in all:
+    list_of_entries = util.list_entries()
+    if title.upper() in list_of_entries:
         new = title.upper()
-    elif title.capitalize() in all:
+    elif title.capitalize() in list_of_entries:
         new = title.capitalize()
     else:
         return HttpResponse("<h1 style=\"color:red\">Error, page not found.</h1>")
@@ -23,4 +23,19 @@ def entry(request, title):
     return render(request, "encyclopedia/Entry.html", {
         "entries": markdown2.markdown(util.get_entry(new)),
         "title": new
+    })
+
+def search(request):
+    if request.method == "POST":
+        word = request.POST['q']
+        if word.upper() in util.list_entries() or word.capitalize() in util.list_entries():
+            return entry(request, word)
+    allitems = []
+    for i in util.list_entries():
+        if i.lower().find(word.lower()) != -1:
+            allitems.append(i)
+    if len(allitems) == 0:
+        return HttpResponse(f"<h1 style=\"color:red\">Your search for ({word}) was not found in wiki encylopedia.</h1>")
+    return render(request, "encyclopedia/index.html", {
+        "entries": allitems
     })
