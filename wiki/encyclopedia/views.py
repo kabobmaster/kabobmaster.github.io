4e35ac834ae17f67bbc4b30a081725e21507cdd8
iamhanantariq <110006315+iamhanantariq@users.noreply.github.com>
2023-01-14 01:03:03 +0000

    entry

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    if title.upper() == "CSS" or title.upper() == "HTML":
+        new = title.upper()
+    else:
+        new = title.capitalize()
+    
+    return render(request, "encyclopedia/Entry.html", {
+        "entries": markdown2.markdown(util.get_entry(new)),
+        "title": new
+    })
