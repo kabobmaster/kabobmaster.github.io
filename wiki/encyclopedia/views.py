efab512166de8aaafc6de24abdd7692e1d843501
iamhanantariq <110006315+iamhanantariq@users.noreply.github.com>
2023-01-14 01:14:51 +0000

    entry done

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
 
@@ -11,11 +12,14 @@ def index(request):
     })
 
 def entry(request, title):
-    if title.upper() == "CSS" or title.upper() == "HTML":
+    all = util.list_entries()
+    if title.upper() in all:
         new = title.upper()
-    else:
+    elif title.capitalize() in all:
         new = title.capitalize()
-    
+    else:
+        return HttpResponse("<h1 style=\"color:red\">Error, page not found.</h1>")
+
     return render(request, "encyclopedia/Entry.html", {
         "entries": markdown2.markdown(util.get_entry(new)),
         "title": new
