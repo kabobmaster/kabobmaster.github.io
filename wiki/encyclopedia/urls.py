a0d6b352fd4c069cb21c948be0cba93584c7d108
iamhanantariq <110006315+iamhanantariq@users.noreply.github.com>
2023-01-15 03:01:10 +0000

    test

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
+    path("newfile", views.newfile, name="newfile"),
+    path("savepage", views.savepage, name="savepage"),
     path("<str:title>", views.entry, name="ENTRY")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
+from django.contrib import messages
 
 from . import util
 
 import markdown2
 
+class newform(forms.Form):
+    formtitle = forms.CharField(label="Title")
+    content = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), label="Markdown Content")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -35,7 +40,36 @@ def search(request):
         if i.lower().find(word.lower()) != -1:
             allitems.append(i)
     if len(allitems) == 0:
-        return HttpResponse(f"<h1 style=\"color:red\">Your search for ({word}) was not found in wiki encylopedia.</h1>")
+            return HttpResponse(f"<h1 style=\"color:red\">Your search for ({word}) was not found in wiki encylopedia.</h1>")
     return render(request, "encyclopedia/index.html", {
         "entries": allitems
+    })
+
+def newfile(request):
+    return render(request, "encyclopedia/newfile.html", {
+        "form": newform()
+    })
+
+def savepage(request):
+        # Check if method is POST
+    if request.method == "POST":
+
+        # Take in the data the user submitted and save it as form
+        form = newform(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["formtitle"]
+            body = form.cleaned_data["content"]
+            if title in util.list_entries():
+                return HttpResponse(f"<h1 style=\"color:red\">Try a new title!</h1>")
+
+            #save and write to md file
+            file = open(f"entries/{title}.md", "w")
+            file.write(f"{body}")
+            file.close()
+
+            #take to the new entry
+
+    return render(request, "encyclopedia/Entry.html", {
+        "entries": markdown2.markdown(util.get_entry(title)),
+        "title": title
     })
