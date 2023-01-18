95fee04593ab08beef26ea1a82780ddfea10679a
iamhanantariq <110006315+iamhanantariq@users.noreply.github.com>
2023-01-18 23:25:49 +0000

    Pending changes exported from your codespace

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class listings(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=1000)
+    bid = models.FloatField(max=1000000)
+    category = models.CharField(max_length=64)
+    image = models.CharField(max_length=1000)
+
+class bids(models.Model):
+    pass
+
+class comments(models.Model):
+    pass
---
+++ wiki/entries/Tariq.md
@@ -0,0 +1,2 @@
+# Title
+## This is a test part 2 to confirm if edit works
